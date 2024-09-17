b4b0dd16c8bdac70bdafad49b72487b5b13a98a3
Lucky Vishwakarma <110558983+LuckyVishwa1104@users.noreply.github.com>
2024-09-17 16:59:53 +0530

    added data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const db = require('./config/db');
 const UserModel = require('./model/user.model');
 const EmployeeModel = require('./model/employee.model');
-
+require('dotenv').config();
 const port = 3000;
 
 app.get('/' , (req,res)=>{
