3785854c8a4b332d1ab5696b3a85f2819034a475
Lucky Vishwakarma <110558983+LuckyVishwa1104@users.noreply.github.com>
2025-02-14 22:26:18 +0530

    added route for update employee API

--- router/employee.router.js
+++ router/employee.router.js
@@ -12,4 +12,6 @@ router.post('/employee', EmployeeController.singleEmployee);
 
 router.post('/deleteEmployee', EmployeeController.deleteEmployee);
 
+router.patch('/updateEmployee', EmployeeController.updateEmployee);
+
 module.exports = router;
