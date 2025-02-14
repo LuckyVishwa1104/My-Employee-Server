96c32988c65c92563142e8531700213a4c5dba94
Lucky Vishwakarma <110558983+LuckyVishwa1104@users.noreply.github.com>
2025-02-14 22:32:24 +0530

    connected to remote database

--- config/db.js
+++ config/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+// mongodb://localhost:27017/myEmployee - connection string for local database
+// mongodb+srv://luckyvishwa1104:lucky1104@cluster0.i72lmi2.mongodb.net/myEmployee?retryWrites=true&w=majority - connection string for remote database
+
 const connection = mongoose.createConnection('mongodb+srv://luckyvishwa1104:lucky1104@cluster0.i72lmi2.mongodb.net/myEmployee?retryWrites=true&w=majority').on('open', ()=>{console.log('MongoDB connected.');
 }).on('error', ()=>{console.log('Error while connecting.');
 });
